3ea1a23cfee728801ae048ad8774783fd3c9c73f
joel magana <joelmagana15@gmail.com>
2024-07-24 14:39:59 +0000

    pascal

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -6,8 +6,11 @@ def pascal_triangle(n):
     
     for i in range(n):
         row = [1] * (i + 1)  # Start with a row of 1s
+        
+        # Update values for the middle elements
         for j in range(1, i):
             row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+        
         triangle.append(row)
     
     return triangle
