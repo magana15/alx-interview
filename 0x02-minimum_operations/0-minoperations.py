8c4afbaaa11956a76dc473cc9b704bb604c5e23a
joel magana <joelmagana15@gmail.com>
2024-08-07 10:34:59 +0000

    min ops

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,20 @@
+"""Module that determines minimum number of operations """
+
+
+def minOperations(n):
+    # init the varible ops
+    ops = 0
+    # init divisor starting with 2
+    divisor = 2
+    # ensure n is greater than 1
+    while n > 1:
+        # look for the divisor that divides completely
+        while n % divisor == 0:
+            # add the divisor to ops
+            ops += divisor
+            # divide n by the divisor to reduce it
+            n //= divisor
+        # otherwise try the next number
+        divisor += 1
+    # returns the nummber of ops once done
+    return ops
