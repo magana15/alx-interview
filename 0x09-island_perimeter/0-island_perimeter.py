8e32cded5a182500362289b52cae750d7a9f1cdd
joel magana <joelmagana15@gmail.com>
2024-09-26 02:19:12 -0700

    perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -21,4 +21,4 @@ def island_perimeter(grid):
                     perimeter += 1
                 if j == cols - 1 or grid[i][j + 1] == 0:
                     perimeter += 1
-                return perimeter
+    return perimeter
