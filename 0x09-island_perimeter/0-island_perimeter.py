67a650653890da185785311aa6184f3f0cc3f83d
joel magana <joelmagana15@gmail.com>
2024-09-26 01:56:35 -0700

    perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -6,16 +6,16 @@ def island_perimeter(grid):
     for i in range(rows):
         for j in range(cols):
             if grid[i][j] == 1:
-                # Check top neighbor
+                
                 if i == 0 or grid[i - 1][j] == 0:
                     perimeter += 1
-                # Check bottom neighbor
+                
                 if i == rows - 1 or grid[i + 1][j] == 0:
                     perimeter += 1
-                # Check left neighbor
+                
                 if j == 0 or grid[i][j - 1] == 0:
                     perimeter += 1
-                # Check right neighbor
+                
                 if j == cols - 1 or grid[i][j + 1] == 0:
                     perimeter += 1
 
